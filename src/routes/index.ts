8e620d8554529ecdb45d0405ec64f058484f8b14
subin <subin012358@gmail.com>
2023-06-05 04:22:19 +0900

    update: rank, team router 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { scheduleRoute } from './schedule.router';
-
+import { teamRoute } from './team.router';
+import { rankRoute } from './rank.router';
 export const v1Router = express.Router();
 
 // v1Router.use('/user', userRoute);
 v1Router.use('/schedule', scheduleRoute);
+v1Router.use('/rank', rankRoute);
+v1Router.use('/team', teamRoute);
