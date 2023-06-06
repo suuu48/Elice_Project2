d1d0b00f34047c24c2fbbd61d69b1c0bbdc5ab8e
subin <subin012358@gmail.com>
2023-06-07 04:22:03 +0900

    update: post, comment 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,15 @@ import express from 'express';
 import { scheduleRoute } from './schedule.router';
 import { teamRoute } from './team.router';
 import { rankRoute } from './rank.router';
+import { postRoute } from './post.router';
+import { commentRoute } from './comment.router';
+
 export const v1Router = express.Router();
 
 // v1Router.use('/user', userRoute);
+v1Router.use('/post', postRoute);
+v1Router.use('/comment', commentRoute);
 v1Router.use('/schedule', scheduleRoute);
 v1Router.use('/rank', rankRoute);
 v1Router.use('/team', teamRoute);
+
