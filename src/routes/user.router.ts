605d397f2b1d5ee9ab2a98d51d22c72e8d9b5b2d
Baecheol <bbaecheol@gmail.com>
2023-06-07 15:52:10 +0900

    update: user.router 수정

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -3,10 +3,10 @@ import * as userController from '../controller/user.controller';
 export const userRoute = express();
 
 // 유저 정보 조회
-userRoute.get('/:email', userController.getUserInfo);
+userRoute.get('/:user_id', userController.getUserInfo);
 
 // 유저 정보 수정
-userRoute.post('/:email', userController.updateUserHandler);
+userRoute.post('/:user_id', userController.updateUserHandler);
 
 // 유저 정보 삭제
-userRoute.delete('/:email', userController.hardDeleteUserHandler);
+userRoute.delete('/:user_id', userController.hardDeleteUserHandler);
