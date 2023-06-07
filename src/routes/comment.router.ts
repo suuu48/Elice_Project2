32db986819e25b2d9d9a10d9afaff16ef0fb4530
subin <subin012358@gmail.com>
2023-06-08 00:31:30 +0900

    update: 댓글 목록

--- src/routes/comment.router.ts
+++ src/routes/comment.router.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import * as commentController from '../controller/comment.controller';
+import {getCommentListHandler} from "../controller/comment.controller";
 
 export const commentRoute = express();
 
 // 댓글 상세 조회
 commentRoute.get('/:comment_id', commentController.getCommentHandler);
+
+//댓글 목록 조회
+commentRoute.get('/list/:id', commentController.getCommentListHandler)
 
 // 댓글 등록 (로그인 필수)
 commentRoute.post('/:user_id', commentController.addCommentHandler);
