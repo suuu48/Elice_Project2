1f70608c5f3a8b1f7b3041724254076b716cc15a
subin <subin012358@gmail.com>
2023-06-08 19:06:24 +0900

    add: cors 추가

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { env } from './config/envconfig';
 import { db } from './config/dbconfig';
 import { v1Router } from './routes';
-
+import cors from 'cors';
 const port = Number(env.PORT);
 const app = express();
 
@@ -19,5 +19,13 @@ db.getConnection()
   .catch((err) => console.log('error!!!!!!!', err));
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // 접근 권한을 부여하는 도메인 ( 5500번 포트 사용 x)
+    credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+    optionsSuccessStatus: 200, // 응답 상태 200으로 설정
+  })
+);
 
 app.use('/api/v1', v1Router);
