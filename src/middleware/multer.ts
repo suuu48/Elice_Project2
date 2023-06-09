788e2bfb9ca2fa16c551ea646b49777e7110c10c
subin <subin012358@gmail.com>
2023-06-10 04:44:04 +0900

    update: shorts랑 img 파일 구분

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -7,19 +7,48 @@ if (!fs.existsSync('public')) {
   fs.mkdirSync('public');
 }
 
-const storage = multer.diskStorage({
+if (!fs.existsSync('public/shorts')) {
+  fs.mkdirSync('public/shorts');
+}
+
+if (!fs.existsSync('public/img')) {
+  fs.mkdirSync('public/img');
+}
+
+const shortsStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/');
+    cb(null, 'public/shorts');
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
 
-const upload = multer({ storage: storage });
+const imgStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
-export const imageData = (req: Request, res: Response, next: NextFunction) => {
-  upload.single('img')(req, res, (error) => {
+const shortsUpload = multer({ storage: shortsStorage });
+const imgUpload = multer({ storage: imgStorage });
+
+export const uploadShorts = (req: Request, res: Response, next: NextFunction) => {
+  shortsUpload.single('src')(req, res, (error) => {
+    try {
+      next();
+    } catch (error) {
+      console.log(error);
+      next(new AppError(400, '[ Shorts 업로드 에러 ] 업로드 실패'));
+    }
+  });
+};
+
+export const uploadImage = (req: Request, res: Response, next: NextFunction) => {
+  imgUpload.single('img')(req, res, (error) => {
     try {
       next();
     } catch (error) {
@@ -28,4 +57,3 @@ export const imageData = (req: Request, res: Response, next: NextFunction) => {
     }
   });
 };
-
