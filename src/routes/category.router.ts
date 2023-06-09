4766e2c51f0066371902475db630f1b7f1e0b7a4
subin <subin012358@gmail.com>
2023-06-10 04:37:45 +0900

    style: 코드 스타일

--- src/routes/category.router.ts
+++ src/routes/category.router.ts
@@ -1,11 +1,8 @@
 import express from 'express';
-import { getCategoriesHandler } from "../controller/category.controller";
+import * as categoryController from '../controller/category.controller';
 export const categoryRoute = express();
 
 // 카테고리 조회
-categoryRoute.get('/', getCategoriesHandler);
-
-// 카테고리 유효성 체크
-//categoryRoute.get('/:categoryId', validateCategory);
+categoryRoute.get('/', categoryController.getCategoriesHandler);
 
 export default categoryRoute;
