1c048dfc805a149db4b2d8b151718630419bdd6c
Baecheol <bbaecheol@gmail.com>
2023-06-10 01:06:18 +0900

    fix: 실수로 push된 cat 부분 제거

--- src/routes/category.router.ts
+++ src/routes/category.router.ts
@@ -5,7 +5,4 @@ export const categoryRoute = express();
 // 카테고리 조회
 categoryRoute.get('/', getCategoriesHandler);
 
-// 카테고리 유효성 체크
-//categoryRoute.get('/:categoryId', validateCategory);
-
 export default categoryRoute;
