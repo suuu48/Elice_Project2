de944598c52dad8788cfa1eaeeb9a88de83b41bd
subin <subin012358@gmail.com>
2023-06-10 04:45:20 +0900

    update: img 추가

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,7 +1,8 @@
 import * as postRepo from '../database/post.repo';
 import * as commentRepo from '../database/comment.repo';
 import { AppError } from '../../../back/src/utils/errorHandler';
-import { Post, createPostInput, updatePostInput } from '../database/schemas/post.entity';
+import { Post, createPostInput, updatePostInput } from '../database/types/post.entity';
+import fs from 'fs';
 
 // 메인페이지 최신글 조회
 export const getPostsMain = async (category: number): Promise<any[]> => {
@@ -88,13 +89,19 @@ export const addPost = async (inputData: createPostInput): Promise<any> => {
 };
 
 // 게시글 수정
-export const editPost = async (post_id: number, updateData: updatePostInput, user_id: number): Promise<any> => {
+export const editPost = async (
+  post_id: number,
+  updateData: updatePostInput,
+  user_id: number
+): Promise<any> => {
   try {
     const isValid = await postRepo.isPostIdValid(post_id);
     if (isValid === false) throw new AppError(404, '존재하는 게시글이 없습니다.');
 
     const isUser = await postRepo.findPostById(post_id);
-    if(isUser.user_id !==user_id) throw new AppError(403, '작성자만 수정 가능합니다.');
+    if (isUser.user_id !== user_id) throw new AppError(403, '작성자만 수정 가능합니다.');
+
+    await editImage(post_id, updateData);
 
     const updatePost = await postRepo.updatePost(post_id, updateData);
     if (updatePost === undefined) throw new AppError(404, '수정된 게시글이 없습니다.');
@@ -120,7 +127,9 @@ export const removePost = async (post_id: number, user_id: number): Promise<numb
     if (isValid === false) throw new AppError(404, '존재하는 게시글이 없습니다.');
 
     const isUser = await postRepo.findPostById(post_id);
-    if(isUser.user_id !==user_id) throw new AppError(403, '작성자만 삭제 가능합니다.');
+    if (isUser.user_id !== user_id) throw new AppError(403, '작성자만 삭제 가능합니다.');
+
+    await removeImage(post_id);
 
     const removedPostId = await postRepo.deletePost(post_id);
 
@@ -131,7 +140,7 @@ export const removePost = async (post_id: number, user_id: number): Promise<numb
       throw error;
     } else {
       console.log(error);
-      throw new AppError(500, '[ 서버 에러 ] 게시글 수정 실패');
+      throw new AppError(500, '[ 서버 에러 ] 게시글 삭제 실패');
     }
   }
 };
@@ -154,7 +163,41 @@ export const reportPost = async (post_id: number): Promise<any> => {
       throw error;
     } else {
       console.log(error);
-      throw new AppError(500, '[ 서버 에러 ] 게시글 수정 실패');
+      throw new AppError(500, '[ 서버 에러 ] 게시글 신고 실패');
     }
   }
+};
+
+/* 게시글 이미지 로컬 수정 */
+const editImage = async (post_id: number, inputData: updatePostInput) => {
+  const foundPost = await postRepo.findPostById(post_id);
+
+  if (foundPost.img && foundPost.img !== inputData.img) {
+    const imgFileName = foundPost.img.split('/')[6];
+
+    const filePath = `/Users/subin/IdeaProjects/back/public/img/${imgFileName}`;
+    // const filePath = `서버 실행하는 로컬의 public 파일 절대경로`;
+    // const filePath = `클라우드 인스턴스 로컬의 public 파일 절대경로`;
+
+    fs.unlink(filePath, (error) => {
+      if (error) throw new AppError(400, '게시글 이미지 수정 중 오류가 발생했습니다.');
+    });
+  } else return;
+};
+
+/* 게시글 이미지 로컬 삭제 */
+const removeImage = async (post_id: number) => {
+  const foundPost = await postRepo.findPostById(post_id);
+
+  if (foundPost.img) {
+    const imgFileName = foundPost.img.split('/')[6];
+    console.log(imgFileName);
+    const filePath = `/Users/subin/IdeaProjects/back/public/img/${imgFileName}`;
+    // const filePath = `서버 실행하는 로컬의 public 파일 절대경로`;
+    // const filePath = `클라우드 인스턴스 로컬의 public 파일 절대경로`;
+
+    fs.unlink(filePath, (error) => {
+      if (error) throw new AppError(400, '게시글 이미지 삭제 중 오류가 발생했습니다.');
+    });
+  } else return;
 };
