5b18f0c2d33c1a7ae44ef92f29af252458173a72
subin <subin012358@gmail.com>
2023-06-10 04:42:29 +0900

    style: 프리티어 적용

--- src/routes/comment.router.ts
+++ src/routes/comment.router.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import * as commentController from '../controller/comment.controller';
-import {getCommentListHandler} from "../controller/comment.controller";
-import {isAccessTokenValid} from "../middleware/jwt";
+import { isAccessTokenValid } from '../middleware/jwt';
 
 export const commentRoute = express();
 
@@ -9,13 +8,17 @@ export const commentRoute = express();
 commentRoute.get('/:comment_id', commentController.getCommentHandler);
 
 //댓글 목록 조회
-commentRoute.get('/list/:id', commentController.getCommentListHandler)
+commentRoute.get('/list/:id', commentController.getCommentListHandler);
 
 // 댓글 등록 (로그인 필수)
-commentRoute.post('/',isAccessTokenValid, commentController.addCommentHandler);
+commentRoute.post('/', isAccessTokenValid, commentController.addCommentHandler);
 
 // 댓글 삭제 (로그인 필수)
 commentRoute.delete('/:comment_id', isAccessTokenValid, commentController.removeCommentHandler);
 
 // 댓글 신고 (로그인 필수)
-commentRoute.patch('/report/:comment_id', isAccessTokenValid, commentController.reportCommentHandler);
+commentRoute.patch(
+  '/report/:comment_id',
+  isAccessTokenValid,
+  commentController.reportCommentHandler
+);
